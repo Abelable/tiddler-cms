8044c2b40afced6d6d039a2339a4c4279b0d1b67
Administrator <admin@example.com>
2022-09-19 17:37:12 +0800

    feat: 角色管理

--- src/screen/authenticated-app/role-list/components/role-modal.tsx
+++ src/screen/authenticated-app/role-list/components/role-modal.tsx
@@ -1,14 +1,27 @@
-import { Form, Input, Modal } from "antd";
+import { Form, Input, Modal, Spin } from "antd";
 import { useForm } from "antd/lib/form/Form";
 import { ErrorBox } from "components/lib";
 import { useAddRole } from "service/role";
 import { useRoleModal, useRolesQueryKey } from "../util";
+import { useEditRole } from "service/role";
+import styled from "@emotion/styled";
+import { useEffect } from "react";
 
-export const RoleModal = ({ type }: { type: number }) => {
+export const RoleModal = () => {
   const [form] = useForm();
-  const { roleModalOpen, editingRoleId, close } = useRoleModal();
+  const { roleModalOpen, editingRoleId, editingRole, isLoading, close } =
+    useRoleModal();
 
-  const { mutateAsync, isLoading, error } = useAddRole(useRolesQueryKey());
+  const useMutateRole = editingRoleId ? useEditRole : useAddRole;
+  const {
+    mutateAsync,
+    isLoading: mutateLoading,
+    error,
+  } = useMutateRole(useRolesQueryKey());
+
+  useEffect(() => {
+    form.setFieldsValue(editingRole);
+  }, [editingRole, form]);
 
   const confirm = () => {
     form.validateFields().then(async () => {
@@ -26,19 +39,41 @@ export const RoleModal = ({ type }: { type: number }) => {
     <Modal
       title={editingRoleId ? "编辑角色" : "新增角色"}
       open={roleModalOpen}
-      confirmLoading={isLoading}
+      confirmLoading={mutateLoading}
       onOk={confirm}
       onCancel={closeModal}
     >
       <ErrorBox error={error} />
-      <Form form={form} layout="vertical">
-        <Form.Item
-          name="content"
-          rules={[{ required: true, message: "请输入常用语" }]}
-        >
-          <Input maxLength={100} placeholder="不超过10字" />
-        </Form.Item>
-      </Form>
+      {isLoading ? (
+        <Loading>
+          <Spin size={"large"} />
+        </Loading>
+      ) : (
+        <Form form={form} layout="vertical">
+          <Form.Item
+            label={"角色名称"}
+            name={"name"}
+            rules={[{ required: true, message: "请输入角色名称" }]}
+          >
+            <Input placeholder={"请输入角色名称"} />
+          </Form.Item>
+          <Form.Item
+            label={"角色描述"}
+            name={"desc"}
+            rules={[{ required: true, message: "请输入角色描述" }]}
+          >
+            <Input placeholder={"请输入角色描述"} />
+          </Form.Item>
+        </Form>
+      )}
     </Modal>
   );
 };
+
+const Loading = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
