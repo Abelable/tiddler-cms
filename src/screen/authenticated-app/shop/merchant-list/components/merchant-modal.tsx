815351b1670c265304e3777d5428c7034aa45cbe
Administrator <admin@example.com>
2023-01-03 15:29:43 +0800

    feat: 商家入驻

--- src/screen/authenticated-app/shop/merchant-list/components/merchant-modal.tsx
+++ src/screen/authenticated-app/shop/merchant-list/components/merchant-modal.tsx
@@ -1,4 +1,4 @@
-import { Descriptions, Drawer } from "antd";
+import { Descriptions, Drawer, Image } from "antd";
 import { ErrorBox, ModalLoading } from "components/lib";
 import dayjs from "dayjs";
 import { useMerchantModal } from "../util";
@@ -25,17 +25,82 @@ export const MerchantModal = () => {
             <Descriptions.Item label="ID">
               {editingMerchant?.id}
             </Descriptions.Item>
-            <Descriptions.Item label="联系人/法人姓名">
-              {editingMerchant?.name}
-            </Descriptions.Item>
-            <Descriptions.Item label="联系人/法人手机号">
-              {editingMerchant?.mobile}
-            </Descriptions.Item>
             <Descriptions.Item label="商家类型">
               {editingMerchant?.type === 1 ? "企业" : "个人"}
             </Descriptions.Item>
             <Descriptions.Item label="注册时间">
               {dayjs(editingMerchant?.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+            </Descriptions.Item>
+            <Descriptions.Item label="更新时间">
+              {dayjs(editingMerchant?.updatedAt).format("YYYY-MM-DD HH:mm:ss")}
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions title="个人信息" size={"small"} column={3}>
+            <Descriptions.Item label="姓名">
+              {editingMerchant?.name}
+            </Descriptions.Item>
+            <Descriptions.Item label="身份证号">
+              {editingMerchant?.idCardNumber}
+            </Descriptions.Item>
+            <Descriptions.Item label="身份证正面照片">
+              <Image width={200} src={editingMerchant?.idCardFrontPhoto} />
+            </Descriptions.Item>
+            <Descriptions.Item label="身份证反面照片">
+              <Image width={200} src={editingMerchant?.idCardFrontPhoto} />
+            </Descriptions.Item>
+            <Descriptions.Item label="手持身份证照片">
+              <Image width={200} src={editingMerchant?.holdIdCardPhoto} />
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions title="联系方式" size={"small"} column={3}>
+            <Descriptions.Item label="手机号">
+              {editingMerchant?.mobile}
+            </Descriptions.Item>
+            <Descriptions.Item label="电子邮箱">
+              {editingMerchant?.email}
+            </Descriptions.Item>
+            <Descriptions.Item label="联系地址">
+              {editingMerchant?.regionList}
+            </Descriptions.Item>
+            <Descriptions.Item label="详细地址">
+              {editingMerchant?.addressDetail}
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions title="企业信息" size={"small"} column={3}>
+            <Descriptions.Item label="企业名称">
+              {editingMerchant?.companyName}
+            </Descriptions.Item>
+            <Descriptions.Item label="企业经营地址">
+              {editingMerchant?.regionList}
+            </Descriptions.Item>
+            <Descriptions.Item label="企业地址详情">
+              {editingMerchant?.addressDetail}
+            </Descriptions.Item>
+            <Descriptions.Item label="营业执照照片">
+              <Image width={200} src={editingMerchant?.businessLicensePhoto} />
+            </Descriptions.Item>
+          </Descriptions>
+          <Descriptions title="法人信息" size={"small"} column={3}>
+            <Descriptions.Item label="姓名">
+              {editingMerchant?.name}
+            </Descriptions.Item>
+            <Descriptions.Item label="手机号">
+              {editingMerchant?.mobile}
+            </Descriptions.Item>
+            <Descriptions.Item label="电子邮箱">
+              {editingMerchant?.email}
+            </Descriptions.Item>
+            <Descriptions.Item label="身份证号">
+              {editingMerchant?.idCardNumber}
+            </Descriptions.Item>
+            <Descriptions.Item label="身份证正面照片">
+              <Image width={200} src={editingMerchant?.idCardFrontPhoto} />
+            </Descriptions.Item>
+            <Descriptions.Item label="身份证反面照片">
+              <Image width={200} src={editingMerchant?.idCardFrontPhoto} />
+            </Descriptions.Item>
+            <Descriptions.Item label="手持身份证照片">
+              <Image width={200} src={editingMerchant?.holdIdCardPhoto} />
             </Descriptions.Item>
           </Descriptions>
         </>
