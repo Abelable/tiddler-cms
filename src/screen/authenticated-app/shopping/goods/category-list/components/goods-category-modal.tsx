b12add0c18c99f61aa961f2a1bb6d8ab463cb33e
Administrator <admin@example.com>
2024-01-03 14:01:09 +0800

    feat: 商品分类

--- src/screen/authenticated-app/shopping/goods/category-list/components/goods-category-modal.tsx
+++ src/screen/authenticated-app/shopping/goods/category-list/components/goods-category-modal.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Select, Modal } from "antd";
+import { Form, Input, InputNumber, Select, Modal } from "antd";
 import { useForm } from "antd/lib/form/Form";
 import { ErrorBox, ModalLoading } from "components/lib";
 import {
@@ -63,18 +63,18 @@ export const GoodsCategoryModal = ({
       ) : (
         <Form form={form} layout="vertical">
           <Form.Item
-            label={"商品分类名称"}
-            name={"name"}
+            label="商品分类名称"
+            name="name"
             rules={[{ required: true, message: "请输入商品分类名称" }]}
           >
             <Input placeholder={"请输入商品分类名称"} />
           </Form.Item>
           <Form.Item
-            name="roleId"
-            label="管理员角色"
-            rules={[{ required: true, message: "请选择管理员角色" }]}
+            name="shopCategoryId"
+            label="所属店铺分类"
+            rules={[{ required: true, message: "请选择所属店铺分类" }]}
           >
-            <Select placeholder="请选择管理员角色">
+            <Select placeholder="请选择所属店铺分类">
               {shopCategoryOptions.map((item) => (
                 <Select.Option key={item.id} value={item.id}>
                   {item.name}
@@ -82,32 +82,82 @@ export const GoodsCategoryModal = ({
               ))}
             </Select>
           </Form.Item>
-
-          <Input.Group compact>
-            <Input
-              style={{ width: 100, textAlign: "center" }}
-              placeholder="Minimum"
-            />
-            <Input
-              className="site-input-split"
-              style={{
-                width: 30,
-                borderLeft: 0,
-                borderRight: 0,
-                pointerEvents: "none",
-              }}
-              placeholder="~"
-              disabled
-            />
-            <Input
-              className="site-input-right"
-              style={{
-                width: 100,
-                textAlign: "center",
-              }}
-              placeholder="Maximum"
-            />
-          </Input.Group>
+          <Form.Item label="销售佣金比例范围%" required>
+            <Input.Group compact>
+              <Form.Item
+                style={{ marginBottom: 0, width: "46%" }}
+                name="minSalesCommissionRate"
+                rules={[{ required: true, message: "请输入最小销售佣金比例" }]}
+              >
+                <InputNumber
+                  style={{ width: "100%", textAlign: "center" }}
+                  placeholder="请输入最小销售佣金比例"
+                />
+              </Form.Item>
+              <Input
+                style={{
+                  width: "8%",
+                  borderRight: 0,
+                  pointerEvents: "none",
+                  textAlign: "center",
+                  backgroundColor: "#fff",
+                }}
+                placeholder="~"
+                disabled
+              />
+              <Form.Item
+                style={{ marginBottom: 0, width: "46%" }}
+                name="maxSalesCommissionRate"
+                rules={[{ required: true, message: "请输入最大销售佣金比例" }]}
+              >
+                <InputNumber
+                  style={{
+                    width: "100%",
+                    textAlign: "center",
+                  }}
+                  placeholder="请输入最大销售佣金比例"
+                />
+              </Form.Item>
+            </Input.Group>
+          </Form.Item>
+          <Form.Item label="推广佣金比例范围%" required>
+            <Input.Group compact>
+              <Form.Item
+                style={{ marginBottom: 0, width: "46%" }}
+                name="minPromotionCommissionRate"
+                rules={[{ required: true, message: "请输入最小推广佣金比例" }]}
+              >
+                <InputNumber
+                  style={{ width: "100%", textAlign: "center" }}
+                  placeholder="请输入最小推广佣金比例"
+                />
+              </Form.Item>
+              <Input
+                style={{
+                  width: "8%",
+                  borderRight: 0,
+                  pointerEvents: "none",
+                  textAlign: "center",
+                  backgroundColor: "#fff",
+                }}
+                placeholder="~"
+                disabled
+              />
+              <Form.Item
+                style={{ marginBottom: 0, width: "46%" }}
+                name="maxPromotionCommissionRate"
+                rules={[{ required: true, message: "请输入最大推广佣金比例" }]}
+              >
+                <InputNumber
+                  style={{
+                    width: "100%",
+                    textAlign: "center",
+                  }}
+                  placeholder="请输入最大推广佣金比例"
+                />
+              </Form.Item>
+            </Input.Group>
+          </Form.Item>
         </Form>
       )}
     </Modal>
